ec662125adf6bed6ba95185b0dfba76b1e641471
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-10-12 19:09:29 +0530

    vendor: Add build finished info
    Change-Id: I6b7a45705bb20280ed71a8226fa0d57530c28747
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    
    -----
    
    vendor: Change MD5 to SHA256
    
    * Now we generate SHA256 instead of MD5
    
    Change-Id: I464fd2540005599681e8a896d95f35721f61145e
    Co-authored-by: xAbdaziz <i@abdaziz.dev>
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -16,12 +16,30 @@
 # -----------------------------------------------------------------
 # MorbidOS OTA update package
 
-MORBID_TARGET_PACKAGE := $(PRODUCT_OUT)/Morbid-$(MORBID_VERSION).zip
+MORBID_TARGET_PACKAGE := $(PRODUCT_OUT)/MorbidOS-$(MORBID_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
+
+CL_PRP="\033[35m"
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(MORBID_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(MORBID_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(MORBID_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(MORBID_TARGET_PACKAGE)" >&2
+	@echo -e ${CL_PRP}""${CL_PRP}
+	@echo -e ${CL_PRP}""${CL_PRP}
+	@echo -e ${CL_PRP}"    __  _______  ____  ____  ________ "${CL_PRP}
+	@echo -e ${CL_PRP}"   /  |/  / __ \/ __ \/ __ )/  _/ __ \"${CL_PRP}
+	@echo -e ${CL_PRP}"  / /|_/ / / / / /_/ / __  |/ // / / /"${CL_PRP}
+	@echo -e ${CL_PRP}" / /  / / /_/ / _, _/ /_/ // // /_/ / "${CL_PRP} 
+	@echo -e ${CL_PRP}"/_/  /_/\____/_/ |_/_____/___/_____/  "${CL_PRP}  
+	@echo -e ${CL_PRP}""${CL_PRP}
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(MORBID_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(MORBID_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(MORBID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(MORBID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
