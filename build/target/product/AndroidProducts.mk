a77d31b33769962886ccd71aedef06965989784d
DroidFreak32 <rushabshah32@gmail.com>
2024-02-12 11:28:40 +0530

    morbid: products: Bring back morbid_sdk_phone_arm64 emulator target
    Change-Id: I0578608bd78e1a03f90b17de4ee558646ce9ef2f
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -27,6 +27,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/morbid_gsi_tv_x86_64.mk \
     $(LOCAL_DIR)/morbid_sdk_car_arm64.mk \
     $(LOCAL_DIR)/morbid_sdk_car_x86_64.mk \
+    $(LOCAL_DIR)/morbid_sdk_phone_arm64.mk \
     $(LOCAL_DIR)/morbid_sdk_phone_x86.mk \
     $(LOCAL_DIR)/morbid_sdk_phone_x86_64.mk \
     $(LOCAL_DIR)/morbid_sdk_tv_arm.mk \
@@ -45,6 +46,7 @@ COMMON_LUNCH_CHOICES := \
     morbid_gsi_tv_x86_64-userdebug \
     morbid_sdk_car_arm64-userdebug \
     morbid_sdk_car_x86_64-userdebug \
+    morbid_sdk_phone_arm64-userdebug \
     morbid_sdk_phone_x86-userdebug \
     morbid_sdk_phone_x86_64-userdebug \
     morbid_sdk_tv_arm-userdebug \
