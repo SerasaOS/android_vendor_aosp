297e402de2e373872c07a4924bd7216d6d424b77
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-07-17 18:01:47 +0530

    config: Build Microsoft apps

--- config/common.mk
+++ config/common.mk
@@ -338,6 +338,9 @@ $(call inherit-product, vendor/custom/config.mk)
 # Optimisation
 $(call inherit-product, vendor/morbid/config/common/optimisation.mk)
 
+# Microsoft
+$(call inherit-product-if-exists, vendor/microsoft/mms/products/mms.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/morbid/config/partner_gms.mk
 -include vendor/morbid-priv/keys/keys.mk
