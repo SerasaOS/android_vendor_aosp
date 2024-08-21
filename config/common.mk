bery-test/vendor_awaken

overlay: Initial RRO overlay for certified props
Make it non-static (but enabled by default) so it can be updated
via adb install.

Change-Id: I6727888e984821b7c50329781bba559963b82002

config/common.mk
@@ -239,10 +239,14 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/awaken/overlay/no-rro
 
 PRODUCT_PACKAGES += \
+    CertifiedPropsOverlay \
     CustomPixelLauncherOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
     NexusLauncherResOverlay
+
+PRODUCT_COPY_FILES += \
+    vendor/awaken/config/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
 
 include vendor/awaken/config/branding.mk
 

SerasaOS/android_vendor_aosp

Signed-off-by: Tiktodz <dotkit@electrowizard.me>

config/common.mk
@@ -242,6 +242,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/MORBID/overlay/no-rro
 
 PRODUCT_PACKAGES += \
+    CertifiedPropsOverlay \
     CustomPixelLauncherOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
@@ -253,6 +254,9 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/morbid/config/bootanimation.mk)
 
 include vendor/morbid/config/branding.mk
+
+PRODUCT_COPY_FILES += \
+    vendor/morbid/config/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/morbid/config/partner_gms.mk