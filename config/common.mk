suvojit213/vendor_pixelstar

vendor: Implement Lineage Health Service and livedisplay
config: Copy Lineage health permissions

Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd
Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

refactor

config/common.mk
@@ -265,6 +265,12 @@ PRODUCT_PACKAGES += \
     NetworkStackOverlay \
     NexusLauncherResOverlay
 
+# Lineage Hardware
+PRODUCT_COPY_FILES += \
+    vendor/pixelstar/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
+    vendor/pixelstar/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
+	vendor/pixelstar/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.livedisplay.xml
+	
 # Bootanimation
 $(call inherit-product, vendor/pixelstar/config/bootanimation.mk)
 

SerasaOS/android_vendor_aosp

Change-Id: I63b3dfe506e7aec8d96adb2ec814f8306e8b21ec
Signed-off-by: Tiktodz <dotkit@electrowizard.me>

config/common.mk
@@ -269,6 +269,12 @@ PRODUCT_PACKAGES += \
     NetworkStackOverlay \
     NexusLauncherResOverlay
 
+# Lineage Hardware
+PRODUCT_COPY_FILES += \
+    vendor/morbid/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
+    vendor/morbid/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
+	vendor/morbid/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.livedisplay.xml
+	
 # Bootanimation
 $(call inherit-product, vendor/morbid/config/bootanimation.mk)
 