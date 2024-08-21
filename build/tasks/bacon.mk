bery-test/vendor_awaken

vendor: tools: Refactor our json script
* Also update build information color.

Change-Id: Idfc991be192f4a9e3c39e098cb4ff2e61444812f
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

build/tasks/bacon.mk
@@ -20,19 +20,18 @@ AWAKEN_TARGET_PACKAGE := $(PRODUCT_OUT)/awaken-$(AWAKEN_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_CYN="\033[36m"
 CL_PRP="\033[35m"
-CL_RED="\033[31m"
-CL_GRN="\033[32m"
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(AWAKEN_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(AWAKEN_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(AWAKEN_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/awaken/tools/generate_json_build_info.sh $(AWAKEN_TARGET_PACKAGE)
-	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
-	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(AWAKEN_TARGET_PACKAGE)${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(AWAKEN_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"ID:"${CL_RED}" `md5sum $(AWAKEN_TARGET_PACKAGE) | cut -d ' ' -f 1`"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Path:"${CL_RED}" $(AWAKEN_TARGET_PACKAGE)"${CL_RST}
-	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
+	echo -e ${CL_BLD}${CL_CYN}"===============================-Package complete-==============================="${CL_CYN}
+	echo -e ${CL_BLD}${CL_CYN}"Datetime :"${CL_PRP}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Size:"${CL_PRP}" `du -sh $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filehash: "${CL_PRP}" `md5sum $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filename: "${CL_PRP} $(AWAKEN_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"ID: "${CL_PRP}" `cat $(AWAKEN_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"================================================================================"${CL_CYN}

SerasaOS/android_vendor_aosp

Signed-off-by: Tiktodz <dotkit@electrowizard.me>

build/tasks/bacon.mk
@@ -20,9 +20,8 @@ MORBID_TARGET_PACKAGE := $(PRODUCT_OUT)/MorbidOS-$(MORBID_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_CYN="\033[36m"
 CL_PRP="\033[35m"
-CL_RED="\033[31m"
-CL_GRN="\033[32m"
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
@@ -37,10 +36,13 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	@echo -e ${CL_PRP}"/_/  /_/\____/_/ |_/_____/___/_____/   "${CL_PRP}  
 	@echo -e ${CL_PRP}""${CL_PRP}
 	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(MORBID_TARGET_PACKAGE)
+	$(hide) $(SHA256) $(MORBID_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(MORBID_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/morbid/tools/generate_json_build_info.sh $(MORBID_TARGET_PACKAGE)
-	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(MORBID_TARGET_PACKAGE)${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(MORBID_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(MORBID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"ID:"${CL_RED}" `md5sum $(MORBID_TARGET_PACKAGE) | cut -d ' ' -f 1`"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Path:"${CL_RED}" $(MORBID_TARGET_PACKAGE)"${CL_RST}
-	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
+	echo -e ${CL_BLD}${CL_CYN}"===============================-Package complete-==============================="${CL_CYN}
+	echo -e ${CL_BLD}${CL_CYN}"Datetime :"${CL_PRP}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Size:"${CL_PRP}" `du -sh $(MORBID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filehash: "${CL_PRP}" `md5sum $(MORBID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filename: "${CL_PRP} $(MORBID_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"ID: "${CL_PRP}" `cat $(MORBID_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"================================================================================"${CL_CYN}