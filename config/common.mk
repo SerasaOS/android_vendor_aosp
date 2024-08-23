PixelStar-dump/vendor_aosp

config: Optimize user builds

config/common.mk
@@ -280,6 +280,9 @@ CUSTOM_LOCALES += \
 # Vendor configurations
 $(call inherit-product, vendor/custom/config.mk)
 
+# Optimisation
+$(call inherit-product, vendor/aosp/config/common/optimisation.mk)
+
 include vendor/aosp/config/version.mk
 
 # OTA

config/common/optimisation.mk
@@ -1,0 +1,52 @@
+# Don't compile SystemUITests
+EXCLUDE_SYSTEMUI_TESTS := true
+
+# Reduce system server verbosity.
+ifeq ($(TARGET_BUILD_VARIANT), user)
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+endif
+
+# Shutdown
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.build.shutdown_timeout?=0
+
+# EGL - Blobcache configuration
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.egl.blobcache.multifile=true \
+    ro.egl.blobcache.multifile_limit=33554432
+
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+
+# Disable MTE Async for system server
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.arm64.memtag.system_server?=off
+
+# Disable Scudo to save RAM and use 32-bit libc variant by default
+PRODUCT_DISABLE_SCUDO ?= true
+MALLOC_SVELTE_FOR_LIBC32 ?= true
+
+# HWUI
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    debug.hwui.use_hint_manager?=true \
+    debug.hwui.target_cpu_time_percent?=30
+
+# Suspend properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    suspend.short_suspend_threshold_millis?=2000 \
+    suspend.max_sleep_time_millis?=40000 \
+    suspend.short_suspend_backoff_enabled?=true
+
+# Watchdog timeout loop breaker
+PRODUCT_PRODUCT_PROPERTIES += \
+    framework_watchdog.fatal_window.second?=600 \
+    framework_watchdog.fatal_count?=3
+
+# Zygote
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    zygote.critical_window.minute?=10
+
+# Inherit art options
+include vendor/aosp/config/common/art.mk

SerasaOS/android_vendor_aosp

Change-Id: Iafd19c14f1b21b80be216030036a166d490c0ab9

config/common.mk
@@ -328,6 +328,9 @@ PRODUCT_COPY_FILES += \
 # Vendor configurations
 $(call inherit-product, vendor/custom/config.mk)
 
+# Optimisation
+$(call inherit-product, vendor/morbid/config/common/optimisation.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/morbid/config/partner_gms.mk
 -include vendor/morbid-priv/keys/keys.mk

config/common/optimisation.mk
@@ -1,0 +1,52 @@
+# Don't compile SystemUITests
+EXCLUDE_SYSTEMUI_TESTS := true
+
+# Reduce system server verbosity.
+ifeq ($(TARGET_BUILD_VARIANT), user)
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+endif
+
+# Shutdown
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.build.shutdown_timeout?=0
+
+# EGL - Blobcache configuration
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.egl.blobcache.multifile=true \
+    ro.egl.blobcache.multifile_limit=33554432
+
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+
+# Disable MTE Async for system server
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.arm64.memtag.system_server?=off
+
+# Disable Scudo to save RAM and use 32-bit libc variant by default
+PRODUCT_DISABLE_SCUDO ?= true
+MALLOC_SVELTE_FOR_LIBC32 ?= true
+
+# HWUI
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    debug.hwui.use_hint_manager?=true \
+    debug.hwui.target_cpu_time_percent?=30
+
+# Suspend properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    suspend.short_suspend_threshold_millis?=2000 \
+    suspend.max_sleep_time_millis?=40000 \
+    suspend.short_suspend_backoff_enabled?=true
+
+# Watchdog timeout loop breaker
+PRODUCT_PRODUCT_PROPERTIES += \
+    framework_watchdog.fatal_window.second?=600 \
+    framework_watchdog.fatal_count?=3
+
+# Zygote
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    zygote.critical_window.minute?=10
+
+# Inherit art options
+include vendor/morbid/config/common/art.mk