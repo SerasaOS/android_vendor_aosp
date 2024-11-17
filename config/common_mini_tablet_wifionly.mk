LineageOS/android_vendor_lineage

config: Ship EmergencyInfo on WiFi only tablets
Change-Id: I89e16f716076d5351949c0bb13f55fc4eaeee4f5

config/common_mini_tablet_wifionly.mk
@@ -4,3 +4,5 @@ $(call inherit-product, vendor/lineage/config/common_mini.mk)
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
+
+$(call inherit-product, vendor/lineage/config/wifionly.mk)

config/wifionly.mk
@@ -1,0 +1,5 @@
+# Apps
+PRODUCT_PACKAGES += \
+    EmergencyInfo
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/wifionly

SerasaOS/android_vendor_aosp

config/common_mini_tablet_wifionly.mk
@@ -4,3 +4,5 @@ $(call inherit-product, vendor/morbid/config/common_mini.mk)
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
+
+$(call inherit-product, vendor/pixelstar/config/wifionly.mk)

config/wifionly.mk
@@ -1,0 +1,5 @@
+# Apps
+PRODUCT_PACKAGES += \
+    EmergencyInfo
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/pixelstar/overlay/wifionly